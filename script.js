d289d94b9c4c0065b6446518cdd775bdf8aa4d43
Adhwaith K P <adhwaithkp8@gmail.com>
2026-02-04 15:27:28 +0530

    finish

---
+++ script.js
@@ -0,0 +1,49 @@
+// Elements
+const envelope = document.getElementById("envelopecontainer");
+const letter = document.getElementById("lettercontainer");
+const noBtn = document.querySelector(".nobutton");
+const yesBtn = document.querySelector(".yesbutton");
+
+const title = document.getElementById("letter-title");
+const catImg = document.getElementById("letter-cat");
+const buttons = document.getElementById("letter-buttons");
+const finalText = document.getElementById("final-text");
+
+// Click Envelope
+
+envelope.addEventListener("click", () => {
+    envelope.style.display = "none";
+    letter.style.display = "flex";
+
+    setTimeout( () => {
+        document.querySelector(".letter-window").classList.add("open");
+    },50);
+});
+
+// Logic to move the NO btn
+
+noBtn.addEventListener("mouseover", () => {
+    const min = 100;
+    const max = 300;
+
+    const distance = Math.random() * (max - min) + min;
+    const angle = Math.random() * Math.PI * 2;
+
+    const moveX = Math.cos(angle) * distance;
+    const moveY = Math.sin(angle) * distance;
+
+    noBtn.style.transition = "transform 0.3s ease";
+    noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
+});
+
+yesBtn.addEventListener("click", () => {
+    title.textContent = "Yippeeee!";
+
+    catImg.src = "cat_dance.gif";
+
+    document.querySelector(".letter-window").classList.add("final");
+
+    buttons.style.display = "none";
+
+    finalText.style.display = "block";
+});
